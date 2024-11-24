4384ebe5d3c03423e237ad0c9628c2d44efeb4b7
SimRuo <133385216+SimRuo@users.noreply.github.com>
2024-11-24 20:35:28 +0100

    Added creation

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -12,8 +12,18 @@ namespace Chirper.Data
     public class AppDbContext : DbContext
     {
         public DbSet<Chirp> Chirps { get; set; }
+        public DbSet<User> Users { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configuring a One-to-Many relationship
+            modelBuilder.Entity<Chirp>()
+                .HasOne(c => c.Author) // A Chirp has one Author
+                .WithMany(u => u.Chirps) // A User has many Chirps
+                .HasForeignKey(c => c.UserId); // UserId is the foreign key
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
